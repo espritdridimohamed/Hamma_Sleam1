14ebb0ea508f6c848257d507913be7d14e6c46d2
Mohamed Dridi <mohamed.dridi@esprit.tn>
2025-11-17 22:43:15 +0100

    Update App.java
    aezaeaeza

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from demo app");
+        System.out.println("HELLO 4 SLEAM1 , worked successfully!");
     }
 }
